d0b60b6892c6dcb5fddb16ad967eee5fd7719346
Benjamin Evans <51272212+dfgHiatus@users.noreply.github.com>
2024-08-05 16:59:54 -0500

    Big bang

---
+++ Config.cs
@@ -0,0 +1,6 @@
+﻿namespace Babble_Bot;
+
+internal class Config
+{
+    public string Token { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using Babble_Bot;
+using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+
+namespace BabbleBot;
+
+internal class Program
+{
+    private const string ResponsesPath = "responses.json";
+    private const string HelpCommand = "!help";
+    private static DiscordSocketClient _client;
+    private Dictionary<string, string> _responses;
+    private readonly string LogFilePath;
+
+    public Program()
+    {
+        // Generate a unique log file name based on the current date and time
+        var timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+        LogFilePath = $"bot_{timestamp}.log";
+    }
+
+    public static void Main(string[] args) => new Program().
+        MainAsync().
+        GetAwaiter().
+        GetResult();
+
+    public async Task MainAsync()
+    {
+        var config = new DiscordSocketConfig
+        {
+            GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMessages | GatewayIntents.MessageContent
+        };
+        _client = new DiscordSocketClient(config);
+
+        _client.Log += Log;
+        _client.MessageReceived += MessageReceivedAsync;
+        LoadResponses();
+
+        var token = JsonConvert.DeserializeObject<Config>(File.ReadAllText("config.json"))!.Token;
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+
+        // Append the log message to the log file
+        File.AppendAllText(LogFilePath, msg.ToString() + Environment.NewLine);
+
+        return Task.CompletedTask;
+    }
+
+    private async Task MessageReceivedAsync(SocketMessage message)
+    {
+        if (message.Author.IsBot) return;
+
+        if (message.Content.StartsWith("!help"))
+        {
+            var command = message.Content.Substring(HelpCommand.Length).Trim();
+            var response = GetHelpResponse(command);
+            await message.Channel.SendMessageAsync(response);
+        }
+    }
+
+    private void LoadResponses()
+    {
+        var json = File.ReadAllText(ResponsesPath);
+        _responses = JsonConvert.DeserializeObject<Dictionary<string, string>>(json)!;
+    }
+
+    private string GetHelpResponse(string command)
+    {
+        if (_responses.TryGetValue(command.ToLower(), out var response))
+        {
+            return response;
+        }
+        else if (_responses.TryGetValue("default", out var defaultResponse))
+        {
+            return defaultResponse;
+        }
+        return "Sorry, I don't have help information for that command.";
+    }
+}
